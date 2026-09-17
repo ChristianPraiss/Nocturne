d37ac87917ab0e5b5ff2cc01a40edbf71d015e07
Jeffser <jsamuel@tuta.io>
2026-09-16 18:20:01 -0600

    ported album row to new bind method

--- src/widgets/album/row.py
+++ src/widgets/album/row.py
@@ -1,13 +1,15 @@
 # row.py
 
-from gi.repository import Gtk, Adw, GLib, Gio, Gdk
-from ...integrations import get_current_integration
+from gi.repository import GObject, Gtk, Adw, GLib, Gio, Gdk
+from ...integrations import get_current_integration, models
 from ...constants import CONTEXT_ALBUM
 from ..containers import ContextContainer
 
 @Gtk.Template(resource_path='/com/jeffser/Nocturne/album/row.ui')
 class AlbumRow(Adw.ActionRow):
     __gtype_name__ = 'NocturneAlbumRow'
+
+    model = GObject.Property(type=models.Album)
 
     cover_el = Gtk.Template.Child()
     menu_button_el = Gtk.Template.Child()
@@ -16,13 +18,9 @@ class AlbumRow(Adw.ActionRow):
         self.id = id
         integration = get_current_integration()
         integration.verifyAlbum(self.id)
-        super().__init__()
-        self.set_action_target_value(GLib.Variant.new_string(self.id))
-
-        integration.connect_to_model(self.id, 'name', self.update_name)
-        integration.connect_to_model(self.id, 'artist', self.update_artist)
-        integration.connect_to_model(self.id, 'gdkPaintable', self.update_cover)
-
+        super().__init__(
+            model=integration.loaded_models.get(self.id)
+        )
         settings = Gio.Settings(schema_id="com.jeffser.Nocturne")
         settings.bind(
             "show-context-button",
@@ -31,20 +29,13 @@ class AlbumRow(Adw.ActionRow):
             Gio.SettingsBindFlags.DEFAULT
         )
 
-    def update_cover(self, paintable):
-        if paintable:
-            self.cover_el.set_from_paintable(paintable)
-            self.cover_el.set_pixel_size(48)
-        elif isinstance(self.cover_el.get_paintable(), Adw.SpinnerPaintable):
-            self.cover_el.set_from_icon_name("music-queue-symbolic")
-            self.cover_el.set_pixel_size(-1)
+    @Gtk.Template.Callback()
+    def format_action_target(self, obj, value, variant) -> GLib.Variant:
+        return GLib.Variant(variant, value)
 
-    def update_name(self, name:str):
-        self.set_title(name)
-        self.set_name(name)
-
-    def update_artist(self, artist:str):
-        self.set_subtitle(artist or "")
+    @Gtk.Template.Callback()
+    def format_cover_pixel_size(self, obj, paintable:Gdk.Paintable) -> int:
+        return 48 if paintable else -1
 
     @Gtk.Template.Callback()
     def on_context_button_active(self, button, gparam):
