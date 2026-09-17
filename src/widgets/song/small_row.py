e82f098a4869934879627a908d62194262472d50
Jeffser <jsamuel@tuta.io>
2026-09-17 07:11:27 -0600

    ported song small_row to new bind method

--- src/widgets/song/small_row.py
+++ src/widgets/song/small_row.py
@@ -1,7 +1,7 @@
 # small_row.py
 
-from gi.repository import Gtk, Adw, Gdk, GLib
-from ...integrations import get_current_integration
+from gi.repository import GObject, Gtk, Adw, Gdk, GLib
+from ...integrations import get_current_integration, models
 from ..containers import ContextContainer
 from ...constants import CONTEXT_SONG
 
@@ -9,52 +9,39 @@ from ...constants import CONTEXT_SONG
 class SongSmallRow(Gtk.Button):
     __gtype_name__ = 'NocturneSongSmallRow'
 
+    model = GObject.Property(type=models.Song)
+    show_album_name = GObject.Property(type=bool, default=False)
+
     cover_el = Gtk.Template.Child()
     title_el = Gtk.Template.Child()
     subtitle_el = Gtk.Template.Child()
 
     def __init__(self, id:str, show_album_name:bool=False):
         self.id = id
-        self.show_album_name = show_album_name
         integration = get_current_integration()
         integration.verifySong(self.id)
         super().__init__(
-            action_target=GLib.Variant.new_string(self.id)
+            model=integration.loaded_models.get(self.id),
+            show_album_name=show_album_name
         )
-        integration.connect_to_model(self.id, 'title', self.update_title)
-        integration.connect_to_model(self.id, 'artists', self.update_artists)
-        integration.connect_to_model(self.id, 'album', self.update_album)
-        integration.connect_to_model(self.id, 'gdkPaintable', self.update_cover)
         integration.connect_to_model(self.id, 'deleted', self.delete_status_changed)
+
+    @Gtk.Template.Callback()
+    def format_action_target(self, obj, value, variant) -> GLib.Variant:
+        return GLib.Variant(variant, value)
+
+    @Gtk.Template.Callback()
+    def format_subtitle(self, obj, album:str, artist:str, show_album_name:bool) -> str:
+        return album if show_album_name else artist
+
+    @Gtk.Template.Callback()
+    def format_pixel_size(self, obj, paintable:Gdk.Paintable) -> int:
+        return 48 if paintable else -1
 
     def delete_status_changed(self, status:bool):
         if status:
             if wrapbox := self.get_ancestor(Adw.WrapBox):
                 wrapbox.remove(self)
-
-    def update_cover(self, paintable):
-        if paintable:
-            self.cover_el.set_from_paintable(paintable)
-            self.cover_el.set_pixel_size(48)
-        elif isinstance(self.cover_el.get_paintable(), Adw.SpinnerPaintable):
-            self.cover_el.set_from_icon_name("music-note-symbolic")
-            self.cover_el.set_pixel_size(-1)
-
-    def update_title(self, title:str):
-        self.title_el.set_label(title)
-        self.set_tooltip_text(title)
-        self.set_name(title)
-
-    def update_artists(self, artists:list):
-        if not self.show_album_name:
-            if len(artists) > 0:
-                self.subtitle_el.set_label(artists[0].get('name'))
-            else:
-                self.subtitle_el.set_label("")
-
-    def update_album(self, album:str):
-        if self.show_album_name:
-            self.subtitle_el.set_label(album)
 
     def generate_context_menu(self) -> ContextContainer:
         integration = get_current_integration()
